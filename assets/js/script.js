c94e3c80f54f8522bd136be7202d1a9ec255f359
Ariel Stancovich <arielstancovichsuarez@gmail.com>
2023-04-01 09:47:32 -0300

    Se agrega desafio

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// funcion para generar los calculos correspondientes junto con su interaccciones
+function operation() {
+    let cantidadIn = document.getElementById("cantidadIn").value;
+    let colorIn = document.getElementById("colorIn").value;
+    let total = cantidadIn*355990;
+    let totalOut = document.getElementById("total");
+    let cantidadOut = document.getElementById("cantidadOut");
+    totalOut.innerHTML = "Total: $"+total;
+    cantidadOut.innerHTML = "Cantidad: "+cantidadIn;
+    document.getElementById("colorOut").style.backgroundColor = colorIn;
+
+}
