b2fdae69fb012175b4a898893fc8e1570c28c253
Ariel Stancovich <arielstancovichsuarez@gmail.com>
2023-04-05 22:00:17 -0400

    modificacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // funcion para generar los calculos correspondientes junto con su interaccciones
 function operation() {
-    let cantidadIn = document.getElementById("cantidadIn").value;
+    let cantidadIn = Number(document.getElementById("cantidadIn").value);
     let colorIn = document.getElementById("colorIn").value;
     let total = cantidadIn*355990;
     let totalOut = document.getElementById("total");
